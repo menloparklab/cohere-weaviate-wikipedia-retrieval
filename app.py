f295833be23952fb16c199ff0b5eaab0b0ff1f39
Misbah Syed <misbah.sy@gmail.com>
2023-05-12 14:00:28 -0700

    chat and parsing

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def compression(k, top_n):
     compressor = CohereRerank(model='rerank-multilingual-v2.0', top_n=top_n )
     compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, base_retriever=retriever)
     return compression_retriever
+
 # This route helps generate answer using Contextual Compression
 @app.route("/retrieve-compr", methods=['POST'])
 def retrieve_compressed_resp():
@@ -78,6 +79,7 @@ def retrieve_compressed_resp():
     qa = RetrievalQA.from_chain_type(llm, retriever=compression_retriever, chain_type_kwargs={"prompt": PROMPT.partial(language=language)})
     search_result = qa({"query": query})
     return {"search_result":search_result}
+
 # This route gets list of most similar embeddings to the questions asked with Contextual Compression
 @app.route("/retrieve-compr-list", methods=['POST'])
 def retrieve_compressed_list():
@@ -87,3 +89,55 @@ def retrieve_compressed_list():
     compression_retriever = compression(k, top_n)
     compressed_docs_list = compression_retriever.get_relevant_documents(query)
     return {"compressed_docs_list":str(compressed_docs_list)}
+
+# Parsing the user input
+from langchain.prompts import PromptTemplate
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+# Defining user input
+class UserInput(BaseModel):
+    question: str = Field(description="question asked by a user")
+    language: str = Field(description="language requested by the user to respond in")
+
+# Set up a parser + inject instructions into the prompt template.
+parser = PydanticOutputParser(pydantic_object=UserInput)
+
+prompt = PromptTemplate(
+    template="Take the user input which contains a question and a language to return results in, and extract the question and language. Extracted question should not have language in it.\n{format_instructions}\n{query}\n",
+    input_variables=["query"],
+    partial_variables={"format_instructions": parser.get_format_instructions()}
+)
+memory = ConversationBufferWindowMemory( k=3, memory_key="chat_history", return_messages=True)
+
+# This route allows to chat with our application without historical chat context
+@app.route("/chat-no-history", methods=['POST'])
+def chat_no_history():
+    query = request.json.get("query")
+    k = request.json.get("k", 9)
+    top_n = request.json.get("top_n", 3)
+    compression_retriever = compression(k, top_n)
+    _input = prompt.format_prompt(query=query)
+    output = llm(_input.to_string())
+    parsed_results = parser.parse(output)
+    language = parsed_results.language or "english"
+    qa = RetrievalQA.from_chain_type(llm, retriever=compression_retriever, chain_type_kwargs={"prompt": PROMPT.partial(language=language)})
+    search_result = qa({"query": parsed_results.question})
+    return {"search_result":search_result['result']}
+
+# This route allows to chat with our application with history
+@app.route("/chat-with-history", methods=['POST'])
+def chat_history():
+    query = request.json.get("query")
+    k = request.json.get("k", 9)
+    top_n = request.json.get("top_n", 3)
+    compression_retriever = compression(k, top_n)
+    _input = prompt.format_prompt(query=query)
+    output = llm(_input.to_string())
+    parsed_results = parser.parse(output)
+    language = parsed_results.language or "english"
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    qa = ConversationalRetrievalChain.from_llm(llm, retriever=compression_retriever,memory=memory, combine_docs_chain_kwargs={"prompt": PROMPT.partial(language=language)})
+    search_result = qa({"question": parsed_results.question})
+    return {"search_result":search_result['answer']}
